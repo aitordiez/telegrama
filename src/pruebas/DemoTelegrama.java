f561613255094db35e1f4d4d1d71273a2c81bab1
Aitor Diez Fernandez <aitorchic@gmail.com>
2018-06-04 19:11:59 +0200

    Prueba para el trabajo de entornos

---
+++ src/pruebas/DemoTelegrama.java
@@ -0,0 +1,35 @@
+/*
+ *DemoTelegrama.java
+ */
+package pruebas;
+
+import objetosNegocio.Telegrama;
+
+/**
+ *
+ * @author aitorchic
+ */
+public class DemoTelegrama {
+
+    /**
+     * Esta funcion prueba los metodos de la clase Telegrama
+     *
+     * @param args Argumentos en la linea de comando
+     */
+    public static void main(String[] args) {
+        Telegrama telegrama1 = new Telegrama("Ordinario", 8);
+        telegrama1.calculaCosto();
+        System.out.println(telegrama1);
+        telegrama1.setNumPalabras(12);
+        telegrama1.calculaCosto();
+        System.out.println(telegrama1);
+        Telegrama telegrama2 = new Telegrama("Urgente", 8);
+        telegrama2.calculaCosto();
+        System.out.println(telegrama2);
+        telegrama2.setNumPalabras(12);
+        telegrama2.calculaCosto();
+        System.out.println(telegrama2);
+    }
+}
+    
+
